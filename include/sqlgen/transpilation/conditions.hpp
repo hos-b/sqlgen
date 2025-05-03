030f599e667eb4d750ebd2ce3ec50f27e9509353
Dr. Patrick Urbanke <patrick@getml.com>
2025-05-03 16:07:04 +0200

    To lhs,rhs

--- include/sqlgen/transpilation/conditions.hpp
+++ include/sqlgen/transpilation/conditions.hpp
@@ -5,8 +5,8 @@ namespace sqlgen::transpilation::conditions {
 
 template <class CondType1, class CondType2>
 struct And {
-  CondType1 cond1;
-  CondType2 cond2;
+  CondType1 lhs;
+  CondType2 rhs;
 };
 
 template <class OpType1, class OpType2>
@@ -47,8 +47,8 @@ struct LesserThan {
 
 template <class CondType1, class CondType2>
 struct Or {
-  CondType1 cond1;
-  CondType2 cond2;
+  CondType1 lhs;
+  CondType2 rhs;
 };
 
 }  // namespace sqlgen::transpilation::conditions
